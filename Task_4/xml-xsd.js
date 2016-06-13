200cd3b30b7a939e2a3c978124546ab951a09b5b
DexterIA <dexterrrrr@list.ru>
2016-06-13 16:15:38 +0300

    minifacate

--- Task_4/xml-xsd.js
+++ Task_4/xml-xsd.js
@@ -2,14 +2,10 @@ var validator = require('xsd-schema-validator'),
   fs = require('fs');
 
 fs.readFile('./testFiles/form.xml', 'utf8', function (err, data) {
-  validate(data.toString());
-});
-
-function validate (xml) {
-  validator.validateXML(xml, './testFiles/schema.xsd', function (err, result) {
+  validator.validateXML(data.toString(), './testFiles/schema.xsd', function (err, result) {
     if (err) {
       throw err;
     }
     console.log(result.valid);
   });
-}
+});
